2a19e51a28c450ac516d54a69ceb1d206f025a1c
Nicholas Lammers <nlammers@BERKELEY.EDU>
2022-05-03 11:17:02 -0700

    Minor changes

--- src/parameter_sweep_scripts/sweeps03_info_vs_cw.m
+++ src/parameter_sweep_scripts/sweeps03_info_vs_cw.m
@@ -42,7 +42,7 @@ for m = 1:length(n_g_vec)
 end    
                       
 % set sim options
-sweep_options = {'n_sim',25,'n_seeds',15,'n_iters_max',50, 'numerical_precision',10, ...
+sweep_options = {'n_sim',200,'n_seeds',15,'n_iters_max',50, 'numerical_precision',10, ...
                 'useParpool',1,'TauCycleTime',1,'downsample_output',ds_flag};
 %%   
 rate_index = find(strcmp(metric_names,'ProductionRate'));
@@ -87,89 +87,3 @@ for equilibrium_flag = 0:1
         end
     end    
 end
-%%
-[ir_max, ir_i] = nanmax(sim_results.metric_array(:,ir_index))
-rates_opt = log10(sim_results.rate_array(ir_i,:))
-
-% close all
-% 
-% figure;
-% hold on
-% for n = 1
-%     
-%     phi_index = find(strcmp(metric_names,'Phi'));
-%     ir_index = find(strcmp(metric_names,'DecisionRateNorm'));
-%     sharpness_index = find(strcmp(metric_names,'Sharpness'));
-%     
-%     % concatenate arrays
-% %     metric_array = [];
-% %     rate_array_long = [];
-% %     for ns_flag = 1:length(results_struct(n).sim_struct)
-% %         metric_array = vertcat(metric_array,results_struct(n).sim_struct(ns_flag).metric_array);
-% %         rate_array_long = vertcat(rate_array_long,results_struct(n).sim_struct(ns_flag).rate_array);
-% %     end
-% %     results_struct(n).sim_struct.metric_array = metric_array;
-% %     results_struct(n).sim_struct.rate_array = rate_array_long;
-%     phi_filter = metric_array(:,phi_index)>=-20;
-%     tau_filter = metric_array(:,tau_index)<=10*3600 & metric_array(:,tau_index)>=0;
-% %     scatter(exp(metric_array(:,phi_index)),metric_array(:,ir_index))    
-%     scatter(exp(metric_array(phi_filter&tau_filter,phi_index)),metric_array(phi_filter&tau_filter,ir_index))    
-% end
-% xlim([1e-4 1e4])
-% % ylim([0 0.03])
-% set(gca,'xscale','log');
-% 
-% %%
-% n = 1;
-% w_flags = contains(results_struct(n).sim_info.sweepVarStrings,'w');
-% sweepFlags = results_struct(n).sim_info.sweepFlags;
-% 
-% tau_index = find(strcmp(metric_names,'CycleTime'));
-% phi_multiplier = (1*results_struct(n).sim_struct.metric_array(:,phi_index)>=0);
-% tau_vec = results_struct(n).sim_struct.metric_array(:,tau_index);
-% tau_multiplier = (1*(tau_vec>=0&tau_vec<=3600*10));
-% [ir_max,ir_i] = nanmax(results_struct(n).sim_struct.metric_array(:,ir_index))
-% tau_c = results_struct(n).sim_struct.metric_array(ir_i,tau_index);
-% rates_raw = results_struct(n).sim_struct.rate_array(ir_i,:)
-% rates_adjusted = log10(rates_raw);
-% % rates_adjusted(sweepFlags&~w_flags) = rates_adjusted(sweepFlags&~w_flags).*tau_c/300;
-% % rates_log = log10(rates_adjusted)
-% 
-% %%
-% [~,~,metric_names] = calculateMetricsNumeric_v3([]);
-% phi_index = find(strcmp(metric_names,'Phi'));
-% ir_index = find(strcmp(metric_names,'DecisionRateNorm'));
-% rate_index = find(strcmp(metric_names,'Production Rate'));
-% 
-% 
-% phi_vec = results_struct(2).metric_array(:,phi_index);
-% ir_vec = results_struct(2).metric_array(:,ir_index);
-% r_vec = results_struct(2).metric_array(:,rate_index);
-% tau_vec =results_struct(2).metric_array(:,tau_index);
-% 
-% [ir_max,ir_i] = nanmax(ir_vec.*(1*phi_vec<=5e2).*(1*phi_vec>=0).*(1*tau_vec<=300))
-% 
-% best_parameters = results_struct(2).rate_array_long(ir_i,:)
-% 
-% %% Look at architectures of sharp networks
-% state_prob_cell = cell(1,length(results_struct));
-% rate_cell = cell(1,length(results_struct));
-% opt_index_vec = NaN(1,length(results_struct));
-% opt_sharpness_vec = NaN(1,length(results_struct));
-% 
-% for n = 1:length(results_struct)
-%     sharp_filter = results_struct(n).sim_struct(1).metric_array(:,sharpness_index)>=0;
-%     % get index of sharpest network
-%     [opt_sharpness_vec(n) ,opt_index_vec(n)] = nanmax(results_struct(n).sim_struct(1).metric_array(:,sharpness_index).*sharp_filter);
-%     % generate network
-%     opt_params = results_struct(n).sim_struct(1).rate_array(opt_index_vec(n),:);
-%     valCell = mat2cell(opt_params,size(opt_params,1),ones(1,size(opt_params,2)));   
-%     % map to appropriate functions
-%     rmpath(genpath('../utilities/metricFunctions/'));
-%     addpath(genpath(results_struct(n).sim_info.functionPath));
-%     % get rate array
-%     rate_cell{n} = RSymFun(valCell{:});
-%     % get probs
-%     state_prob_cell{n} = calculate_ss_num(rate_cell{n},10);      
-% end    
-% 
